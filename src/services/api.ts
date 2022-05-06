8b183a8e92073c7f71f42665ec40750f88328661
Breno Borges <bgfborges@gmail.com>
2022-05-05 21:43:30 -0300

    Basic Error Handling

--- src/services/api.ts
+++ src/services/api.ts
@@ -32,11 +32,16 @@ export class StoreAPI {
 
         const endpoint = amount > 0 ? `/categories?page=1&limit=${amount}` : '/categories'
 
-        // Return the Categories List with the opinional amount set
-        const response = await this.api.get(endpoint)
-        const categories: Category[] = response.data
+        // Basic Error Handling - Further Improvements
+        try {
+            // Return the Categories List with the opinional amount set
+            const response = await this.api.get(endpoint)
+            const categories: Category[] = response.data
 
-        return categories
+            return categories
+        } catch(e){
+            console.log(e);
+        }
     }
     
     public async getCategory(categoryId: string): Promise<Category> {
@@ -44,11 +49,16 @@ export class StoreAPI {
 
         const endpoint =`/categories/${categoryId}`
 
-        // Return the Categories List with the opinional amount set
-        const response = await this.api.get(endpoint)
-        const category: Category = response.data
+        // Basic Error Handling - Further Improvements
+        try {
+            // Return the Categories List with the opinional amount set
+            const response = await this.api.get(endpoint)
+            const category: Category = response.data
 
-        return category
+            return category
+        } catch(e) {
+            console.log(e)
+        }
     }
     
     public async getProducts(categoryId: string, amount?: number): Promise<Product[]> {
@@ -56,47 +66,62 @@ export class StoreAPI {
 
         const endpoint = amount > 0 ? `/categories/${categoryId}/products?page=1&limit=${amount}` : `/categories/${categoryId}/products`
 
-        // Return the Categories List with the opinional amount set
-        const response = await this.api.get(endpoint)
-        const products: Product[] = response.data
+        // Basic Error Handling - Further Improvements
+        try {
+            // Return the Categories List with the opinional amount set
+            const response = await this.api.get(endpoint)
+            const products: Product[] = response.data
 
-        return products    
+            return products
+        } catch(e) {
+            console.log(e)
+        }
     }
 
     public async getProduct(categoryId: any, productId: any): Promise<Product | null> {
 
         const endpoint = `/categories/${categoryId}/products/${productId}`
 
-        // Return the Categories List with the opinional amount set
-        const response = await this.api.get(endpoint)
-        const product: Product = response.data;
+        // Basic Error Handling - Further Improvements
+        try{
+            // Return the Categories List with the opinional amount set
+            const response = await this.api.get(endpoint)
+            const product: Product = response.data;
 
-        // The Product endpoint doesn't return the category name from API, so for this situation, it's necessary to make a second call to get it
-        // In production with a real API (not this mock.io) its necessary to implement better
+            // The Product endpoint doesn't return the category name from API, so for this situation, it's necessary to make a second call to get it
+            // In production with a real API (not this mock.io) its necessary to implement better
 
-        const categoryFromThisProduct = await this.getCategory(product.categoryId);
-        const { name: categoryName } = categoryFromThisProduct;
+            const categoryFromThisProduct = await this.getCategory(product.categoryId);
+            const { name: categoryName } = categoryFromThisProduct;
 
-        // Assign Category Name to Product
-        product.category = categoryName;
+            // Assign Category Name to Product
+            product.category = categoryName;
 
-        // Format the Price
-        product.price = new Intl.NumberFormat('es-US', {
-            style: 'currency',
-            currency: 'BRL',
-            currencyDisplay: 'narrowSymbol'
-          }).format(Number(product.price) / 100)
+            // Format the Price
+            product.price = new Intl.NumberFormat('es-US', {
+                style: 'currency',
+                currency: 'BRL',
+                currencyDisplay: 'narrowSymbol'
+            }).format(Number(product.price) / 100)
 
-        return product
+            return product
+        } catch(e){
+            console.log(e)
+        }
     }
 
     public async postOrder(order: Order): Promise<void> {
 
-        await this.api({
-            method: 'post',
-            url: '/orders',
-            data: order
-        })
+        // Basic Error Handling - Further Improvements
+        try{
+            await this.api({
+                method: 'post',
+                url: '/orders',
+                data: order
+            })
+        } catch(e){
+            console.log(e)
+        }
     }
 
 }
